ed30b9184d2d8701139f6b70c873e1641f673614
jhonLukas <lucasrochalisb@gmail.com>
2025-02-12 22:53:05 -0300

    Get by id

--- src/main/java/com/joni/Gamelist/Controllers/GameController.java
+++ src/main/java/com/joni/Gamelist/Controllers/GameController.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joni.Gamelist.Services.GameService;
+import com.joni.Gamelist.dto.GameDTO;
 import com.joni.Gamelist.dto.GameMinDTO;
-import com.joni.Gamelist.entities.Game;
 
 @RestController
-@RequestMapping("/games") // Correção aqui
+@RequestMapping("/games") 
 public class GameController {
     
     @Autowired
@@ -21,6 +22,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
     	  List<GameMinDTO> result = gameService.findAll();
+    	  return result;
+    }
+
+    @GetMapping(value ="/{id}" )
+    public GameDTO findByid(@PathVariable Long id) {
+    	  GameDTO result = gameService.findByid(id);
     	  return result;
     }
 }
--- src/main/java/com/joni/Gamelist/Services/GameService.java
+++ src/main/java/com/joni/Gamelist/Services/GameService.java
@@ -1,24 +1,33 @@
 package com.joni.Gamelist.Services;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joni.Gamelist.Repositories.GameRepository;
+import com.joni.Gamelist.dto.GameDTO;
 import com.joni.Gamelist.dto.GameMinDTO;
 import com.joni.Gamelist.entities.Game;
 
 @Service
 public class GameService {
-    
-    @Autowired
-    private GameRepository gameRepository;  
 
-    public List<GameMinDTO> findAll() {
-        List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-        return dto;
-    }
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional(readOnly = true)
+	public GameDTO findByid(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
 }
